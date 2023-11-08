695037be98deadfc7a6591c953526a0122fc902f
chriscunningham-cts <chris.cunningham@cts.co>
2023-11-08 16:00:25 +0000

    try appending to bashrc instead

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mkdir -p "$HOME/.local/bin"
 
 cp -rp config/* "$HOME/.config/"
 
-cp bash_profile "$HOME/.bash_profile"
+cat bash_profile >>"$HOME/.bashrc"
 
 curl -SOL https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-v0.24.0-aarch64-unknown-linux-gnu.tar.gz &&
   tar zxf bat-v0.24.0-aarch64-unknown-linux-gnu.tar.gz &&
