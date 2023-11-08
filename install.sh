a28dfb6133896f222cbd8f004b151844721ebb65
chriscunningham-cts <chris.cunningham@cts.co>
2023-11-08 15:35:33 +0000

    see if this works

--- install.sh
+++ install.sh
@@ -4,4 +4,13 @@ set -e
 
 cd "$(dirname "$0")"
 
-cp -rp config/* ~/.config/
+mkdir -p "$HOME/.local/bin"
+
+cp -rp config/* "$HOME/.config/"
+
+cp profile "$HOME/.profile"
+
+curl -SOL https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-v0.24.0-aarch64-unknown-linux-gnu.tar.gz &&
+  tar zxf bat-v0.24.0-aarch64-unknown-linux-gnu.tar.gz &&
+  install bat-v0.24.0-aarch64-unknown-linux-gnu/bat "$HOME/.local/bin/" &&
+  rm -rf bat-v0.24.0-aarch64-unknown-linux-gnu* bat
