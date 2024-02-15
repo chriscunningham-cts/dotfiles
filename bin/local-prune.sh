2547278a22225bf500f4696db10aeb18a9d93a77
chriscunningham-cts <chris.cunningham@cts.co>
2024-02-15 13:38:00 +0100

    allow non-master default branch

--- bin/local-prune.sh
+++ bin/local-prune.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-git checkout -q master && git for-each-ref refs/heads/ "--format=%(refname:short)" |
+git checkout -q ${1:-main} && git for-each-ref refs/heads/ "--format=%(refname:short)" |
   while read branch; do
-    mergeBase=$(git merge-base master "$branch") && [[ $(git cherry master $(git commit-tree $(git rev-parse "$branch^{tree}") -p "$mergeBase" -m _)) == "-"* ]] && git branch -D "$branch"
+    mergeBase=$(git merge-base ${1:-main} "$branch") && [[ $(git cherry ${1:-main} $(git commit-tree $(git rev-parse "$branch^{tree}") -p "$mergeBase" -m _)) == "-"* ]] && git branch -D "$branch"
   done
