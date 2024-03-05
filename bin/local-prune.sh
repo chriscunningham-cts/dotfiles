4675d1e8df48794bd784e4722c593a32421aa424
chriscunningham-cts <chris.cunningham@cts.co>
2024-03-05 15:41:18 +0100

    new alias

--- bin/local-prune.sh
+++ bin/local-prune.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-git checkout -q ${1:-main} && git for-each-ref refs/heads/ "--format=%(refname:short)" |
-  while read branch; do
-    mergeBase=$(git merge-base ${1:-main} "$branch") && [[ $(git cherry ${1:-main} $(git commit-tree $(git rev-parse "$branch^{tree}") -p "$mergeBase" -m _)) == "-"* ]] && git branch -D "$branch"
+git checkout -q "${1:-main}" && git for-each-ref refs/heads/ "--format=%(refname:short)" |
+  while read -r branch; do
+    mergeBase=$(git merge-base "${1:-main}" "$branch") && [[ $(git cherry "${1:-main}" $(git commit-tree $(git rev-parse "$branch^{tree}") -p "$mergeBase" -m _)) == "-"* ]] && git branch -D "$branch"
   done
